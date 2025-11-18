a677b8ef8f231814019ede1b2c47aaefcfe6a827
bboogaard <bboogaard1971@gmail.com>
2025-11-18 12:15:41 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(name='django-trix-widget',
          'pytest',
          'pytest-cov',
          'pytest-django~=4.5.2',
-         'django~=3.2.23',
+         'django==4.2.7',
          'pyquery~=2.0.0',
          'bleach~=6.1.0'
      ],
